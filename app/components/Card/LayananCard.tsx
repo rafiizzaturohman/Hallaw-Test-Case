cd84ea4a1dc1524de1d10f572aa432d38a764dfd
Rafi Izzaturohman <53922794+rafiizzaturohman@users.noreply.github.com>
2023-09-06 07:29:49 +0700

    Hallaw

--- app/components/Card/LayananCard.tsx
+++ app/components/Card/LayananCard.tsx
@@ -12,7 +12,7 @@ const LayananCard: React.FC<LayananCardProps> = ({
   descriptions,
 }) => {
   return (
-    <div className="container my-4">
+    <div className="container my-4 md:mx-2">
       <div className="bg-white shadow-xl shadow/50 h-44 md:h-[466px] md:w-[334px] px-6 py-10 rounded-lg">
         <div className="flex flex-row md:flex-col justify-evenly gap-4 items-center md:items-start md:space-y-4">
           <div className="flex justify-center items-center bg-[#F1F3FA] w-40 h-16 md:w-24 md:h-24 p-2 md:p-4 rounded-lg">
