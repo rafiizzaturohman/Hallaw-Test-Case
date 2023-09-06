ba91c022962e09859cfc79c7eca2ce2b5d53a383
Rafi Izzaturohman <53922794+rafiizzaturohman@users.noreply.github.com>
2023-09-07 06:01:38 +0700

    Hallaw

--- app/components/LayananDarurat.tsx
+++ app/components/LayananDarurat.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const LayananDarurat: React.FC = () => {
   return (
-    <div className="fixed z-40 bottom-8 right-4 md:bottom-14 md:right-40">
+    <div className="fixed z-40 bottom-8 right-4 md:bottom-14 md:right-10">
       <Link href="https://www.whatsapp.com">
         <div className="bg-gradient-to-br from-[#28E776] to-[#01D160] rounded-full cursor-pointer">
           <div className="flex flex-row items-center gap-1 md:gap-2 px-3 py-2 md:px-8 md:py-3">
